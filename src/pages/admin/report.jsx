c43ad608e4d9ccea0d6dba4f59edc54ee32c6be0
ver2ex1 <vadym.hl@darly.solutions>
2023-08-05 20:19:12 +0300

    add layout

---
+++ src/pages/admin/report.jsx
@@ -0,0 +1,9 @@
+import AdminLayout from '@/components/common/AdminLayout';
+
+const { Box } = require('@mui/material');
+
+const AdminReport = () => {
+  return <AdminLayout>123</AdminLayout>;
+};
+
+export default AdminReport;
